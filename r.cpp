75bad537992a690b72ac41dc6eb4de7d38fcbc52
ROHIT KUMAR PATEL <105638346+luky2001@users.noreply.github.com>
2022-08-20 11:53:05 +0530

    Add files via upload

---
+++ r.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+ #ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+
+
+int a=9;int b=5;
+cout<< a+b <<endl;
+
+
+
+
+
+}
